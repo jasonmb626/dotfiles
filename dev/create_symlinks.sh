25143b27d762783393f08b6cf2b2c7b0390b0236
jasonmb626@gmail.com <jason@jasonbrunelle.com>
2021-04-13 07:35:46 -0500

    Fix SDDM theme directory, symlink typo

--- dev/create_symlinks.sh
+++ dev/create_symlinks.sh
@@ -4,5 +4,5 @@ ln -sf /home/jason/src/dotfiles/dev/config .config
 ln -sf /home/jason/src/dotfiles/dev/local .local
 ln -sf /home/jason/src/dotfiles/dev/kde4 .kde4
 ln -sf /home/jason/src/dotfiles/dev/themes .themes
-ln -sf /home/jason/src/dotfiles/dev/gtkrc-2.0 .gtkr-2.0
+ln -sf /home/jason/src/dotfiles/dev/gtkrc-2.0 .gtkrc-2.0
 ln -sf /home/jason/src/dotfiles/dev/local/share/themes/sddm/Nordic /usr/share/sddm/themes/Nordic
