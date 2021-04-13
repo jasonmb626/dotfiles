9365f545e72c45afc1063e2faf19f54d2441b7eb
jasonmb626@gmail.com <jason@jasonbrunelle.com>
2021-04-13 08:01:15 -0500

    Fix SDDM not defaulting to correct theme

--- dev/create_symlinks.sh
+++ dev/create_symlinks.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+rm -fr .config
+rm -fr .local
+rm -fr .kde4
+rm -fr .themes
+rm .gtkrc-2.0
+rm -fr /usr/share/sddm/themes/Nordic
+
 ln -sf /home/jason/src/dotfiles/dev/config .config
 ln -sf /home/jason/src/dotfiles/dev/local .local
 ln -sf /home/jason/src/dotfiles/dev/kde4 .kde4
 ln -sf /home/jason/src/dotfiles/dev/themes .themes
 ln -sf /home/jason/src/dotfiles/dev/gtkrc-2.0 .gtkrc-2.0
 ln -sf /home/jason/src/dotfiles/dev/local/share/themes/sddm/Nordic /usr/share/sddm/themes/Nordic
+
+echo "Also change /etc/sddm.conf.d/kde_settings.conf"
+echo ""
+echo "[Theme]"
+echo "Current=Nordic"
