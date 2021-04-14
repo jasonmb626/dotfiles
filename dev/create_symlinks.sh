9280c8fc650b3b341b90680daad6c3ce4becab1f
jasonmb626@gmail.com <jason@jasonbrunelle.com>
2021-04-14 09:51:50 -0500

    Window gaps. Virtual-desktop-bar

--- dev/create_symlinks.sh
+++ dev/create_symlinks.sh
@@ -13,7 +13,7 @@ ln -sf /home/jason/src/dotfiles/dev/kde4 .kde4
 ln -sf /home/jason/src/dotfiles/dev/themes .themes
 ln -sf /home/jason/src/dotfiles/dev/gtkrc-2.0 .gtkrc-2.0
 ln -sf /home/jason/src/dotfiles/dev/local/share/themes/sddm/Nordic /usr/share/sddm/themes/Nordic
-
+ln -sf /home/jason/src/dotfiles/dev/kde/share/app/virtualdesktopbar /usr/lib/qt/qml/org/kde/plasma/virtualdesktopbar
 echo "Also change /etc/sddm.conf.d/kde_settings.conf"
 echo ""
 echo "[Theme]"
