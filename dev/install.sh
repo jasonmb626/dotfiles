df0b685533fda5b2cbd3b3277ce386374b5c054e
jasonmb626@gmail.com <jason@jasonbrunelle.com>
2021-04-14 10:21:01 -0500

    Fix install.sh

---
+++ dev/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -fr /home/jason/.config
+rm -fr /home/jason/.local
+rm -fr /home/jason/.kde4
+rm -fr /home/jason/.themes
+rm /home/jason/.gtkrc-2.0
+
+ln -sf /home/jason/git/dotfiles/dev/config /home/jason/.config
+ln -sf /home/jason/git/dotfiles/dev/local /home/jason/.local
+ln -sf /home/jason/git/dotfiles/dev/kde4 /home/jason/.kde4
+ln -sf /home/jason/git/dotfiles/dev/themes /home/jason/.themes
+ln -sf /home/jason/git/dotfiles/dev/gtkrc-2.0 /home/jason/.gtkrc-2.0
+cp -a /home/jason/git/dotfiles/dev/kde/share/app/virtualdesktopbar /usr/lib/qt/qml/org/kde/plasma/virtualdesktopbar
+cp -a /home/jason/git/dotfiles/dev/local/share/themes/sddm/Nordic /usr/share/sddm/themes/Nordic
+cp /home/jason/git/dotfiles/dev/kde_settings.conf /etc/sddm.conf.d/
