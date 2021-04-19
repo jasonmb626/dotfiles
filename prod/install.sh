a4b84ff47dec285ba8d42c7afa4c331b8ffc0d56
Jason Brunelle <jason@jasonbrunelle.com>
2021-04-19 06:49:57 -0500

    Fix code-oss link. Termite config

--- prod/install.sh
+++ prod/install.sh
@@ -4,5 +4,5 @@ ln -sf /home/jason/git/dotfiles/prod/local/share/icons /home/jason/.icons
 ln -sf /home/jason/git/dotfiles/prod/local/share/fonts /home/jason/.local/share/fonts
 ln -sf /home/jason/git/dotfiles/prod/themes /home/jason/.themes
 ln -sf /home/jason/git/dotfiles/prod/vscode-oss /home/jason/.vscode-oss
-ln -sf /home/jason/git/dotfiles/prod/.config/code-oss /home/jason/.config/code-oss
+ln -sf /home/jason/git/dotfiles/prod/.config/'Code - OSS' /home/jason/.config/code-oss
 ln -sf /home/jason/git/dotfiles/prod/.config/termite /home/jason/.config/termite
