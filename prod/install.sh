463331ed2b92f5e9afab024aab7856f68fd06a94
Jason Brunelle <jason@jasonbrunelle.com>
2021-04-19 06:34:31 -0500

    Add prod configs

---
+++ prod/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf /home/jason/git/dotfiles/prod/local/share/icons /home/jason/.icons
+ln -sf /home/jason/git/dotfiles/prod/local/share/fonts /home/jason/.local/share/fonts
+ln -sf /home/jason/git/dotfiles/prod/themes /home/jason/.themes
+ln -sf /home/jason/git/dotfiles/prod/vscode-oss /home/jason/.vscode-oss
+ln -sf /home/jason/git/dotfiles/prod/.config/code-oss /home/jason/.config/code-oss
+ln -sf /home/jason/git/dotfiles/prod/.config/termite /home/jason/.config/termite
