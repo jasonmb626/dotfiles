b9f7b5b69f2607e01cc9f4339e6dcee54786cdad
Jason Brunelle <jason@jasonbrunelle.com>
2021-04-19 07:31:38 -0500

    Fix P10k config. Add nerd fonts

--- prod/install.sh
+++ prod/install.sh
@@ -6,3 +6,6 @@ ln -sf /home/jason/git/dotfiles/prod/themes /home/jason/.themes
 ln -sf /home/jason/git/dotfiles/prod/vscode-oss /home/jason/.vscode-oss
 ln -sf /home/jason/git/dotfiles/prod/.config/'Code - OSS' /home/jason/.config/code-oss
 ln -sf /home/jason/git/dotfiles/prod/.config/termite /home/jason/.config/termite
+
+#also copy /home/jason/git/dotfiles/prod/local/share/zshconfig/plugins to /usr/share/zsh and set it all root.root.
+#also copy /home/jason/git/dotfiles/prod/local/share/zshconfig/zsh-theme-powerlevel10k to /usr/share and set it all root.root.
