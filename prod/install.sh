5afa0d70735826e5343d7dc4067d6901f4ddf7d2
Jason Brunelle <jason@jasonbrunelle.com>
2021-04-21 23:07:35 -0500

    Fix zsh prompt. Fix vscode config path

--- prod/install.sh
+++ prod/install.sh
@@ -4,9 +4,16 @@ ln -sf /home/jason/git/dotfiles/prod/local/share/icons /home/jason/.icons
 ln -sf /home/jason/git/dotfiles/prod/local/share/fonts /home/jason/.local/share/fonts
 ln -sf /home/jason/git/dotfiles/prod/themes /home/jason/.themes
 ln -sf /home/jason/git/dotfiles/prod/vscode-oss /home/jason/.vscode-oss
-ln -sf /home/jason/git/dotfiles/prod/.config/'Code - OSS' /home/jason/.config/code-oss
-ln -sf /home/jason/git/dotfiles/prod/.config/termite /home/jason/.config/termite
+ln -sf /home/jason/git/dotfiles/prod/config/'Code - OSS' /home/jason/.config/code-oss
+ln -sf /home/jason/git/dotfiles/prod/config/termite /home/jason/.config/termite
 ln -sf /home/jason/git/dotfiles/prod/pam_environment /home/jason/.pam_environment
-
-#also copy /home/jason/git/dotfiles/prod/local/share/zshconfig/plugins to /usr/share/zsh and set it all root.root.
-#also copy /home/jason/git/dotfiles/prod/local/share/zshconfig/zsh-theme-powerlevel10k to /usr/share and set it all root.root.
+ln -sf /home/jason/git/dotfiles/prod/zshrc /home/jason/.zshrc
+ 
+cp -a /home/jason/git/dotfiles/prod/local/share/zshconfig/plugins /usr/share/zsh
+chown -R root.root /usr/share/zsh/plugins
+cp -a /home/jason/git/dotfiles/prod/local/share/zshconfig/zsh-theme-powerlevel10k /usr/share
+chown -R root.root /usr/share/zsh-theme-powerlevel10k
+cp /home/jason/git/dotfiles/prod/local/share/zshconfig/manjaro-zsh-config /usr/share/zsh
+chown root.root /usr/share/zsh/manjaro-zsh-config 
+cp /home/jason/git/dotfiles/prod/local/share/zshconfig/manjaro-zsh-prompt /usr/share/zsh
+chown root.root /usr/share/zsh/manjaro-zsh-prompt
