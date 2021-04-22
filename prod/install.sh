6591ed801e6569c3f40887283db27f7618ab2e85
Jason Brunelle <jason@jasonbrunelle.com>
2021-04-21 23:11:35 -0500

    Add p10k config to repository.

--- prod/install.sh
+++ prod/install.sh
@@ -8,6 +8,7 @@ ln -sf /home/jason/git/dotfiles/prod/config/'Code - OSS' /home/jason/.config/cod
 ln -sf /home/jason/git/dotfiles/prod/config/termite /home/jason/.config/termite
 ln -sf /home/jason/git/dotfiles/prod/pam_environment /home/jason/.pam_environment
 ln -sf /home/jason/git/dotfiles/prod/zshrc /home/jason/.zshrc
+ln -sf /home/jason/git/dotfiles/prod/p10k.zsh /home/jason/.p10k.zsh
  
 cp -a /home/jason/git/dotfiles/prod/local/share/zshconfig/plugins /usr/share/zsh
 chown -R root.root /usr/share/zsh/plugins
